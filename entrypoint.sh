69a237f3e2607099ba41ae68dbff4d7b27c831bd
quinqu <quinq@goteleport.com>
2021-12-06 17:07:33 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,8 @@ env
 go run /main.go
 
 cd $RUNNER_WORKSPACE
-
+git config user.name github-actions
+git config user.email github-actions@github.com
 git fetch 
 git commit -m "test" --allow-empty
 git push 
